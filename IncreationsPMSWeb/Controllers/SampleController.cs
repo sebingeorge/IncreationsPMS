795ec2e13ffed511bd73f370c4d05b4138fd2d39
Jinu John <jinu@abc.com>
2016-12-08 17:50:17 +0530

    dchanges

---
+++ IncreationsPMSWeb/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IncreationsPMSWeb.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SampleD()
+        {
+            return View();
+        }
+    }
+}
