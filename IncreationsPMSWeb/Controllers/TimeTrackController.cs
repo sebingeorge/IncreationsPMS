0e0c166243fb0597585918d1483bb7f876ff61dd
Jinu John <jinu@abc.com>
2016-12-09 14:41:10 +0530

    Dchanges

---
+++ IncreationsPMSWeb/Controllers/TimeTrackController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IncreationsPMSWeb.Controllers
+{
+    public class TimeTrackController : Controller
+    {
+        // GET: TimeTrack
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TimeTrack()
+        {
+            return View();
+        }
+    }
+}
